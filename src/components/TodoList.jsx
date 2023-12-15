758b60700b5a807880a70295609016bf6c88f325
Adrián Ucha Sousa <adrii.ucha03@gmail.com>
2023-12-15 12:42:57 +0100

    modified:   src/components/TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,37 @@
 import Todo from './Todo';
 
-const TodoList = ({ todos, deleteTodo , updateTodo, editTodo}) => {
+const TodoList = ({ todos, deleteTodo, updateTodo, editTodo }) => {
+  // Función de comparación para ordenar las tareas
+  const compareTasks = (a, b) => {
+    // Primero, colocar las tareas completadas al final, independientemente de la prioridad
+    if (a.state && !b.state) {
+      return 1;
+    }
+    if (!a.state && b.state) {
+      return -1;
+    }
+  
+    // Segundo, ordenar por prioridad (tareas con prioridad primero)
+    if (b.priority && !a.priority) {
+      return 1;
+    }
+    if (a.priority && !b.priority) {
+      return -1;
+    }
+  
+    // Finalmente, ordenar por ID (si tienen la misma prioridad y estado)
+    return a.id - b.id;
+  };
+
+  // Ordenar las tareas usando la función de comparación
+  const sortedTodos = [...todos].sort(compareTasks);
+
   return (
     <div>
-    <h1 className='text-center'>Lista de tareas</h1>
+      <h1 className='text-center'>Lista de tareas</h1>
       <ul>
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} updateTodo={updateTodo} editTodo={editTodo}/>
+        {sortedTodos.map((todo) => (
+          <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} updateTodo={updateTodo} editTodo={editTodo} />
         ))}
       </ul>
     </div>
